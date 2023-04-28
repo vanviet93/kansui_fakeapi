50214afb24ad6c877bc745cc3a5ce44f9c36105f
vanviet93 <phamvanviet93@gmail.com>
2023-04-28 16:28:02 +0900

    nothing

---
+++ server.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const fs = require('fs');
+const moment = require('moment')
+const app = express();
+const port = 3000;
+
+const A = 0.68, B = 5.88, C = 0.68, D = 5.88, E = 45, F = -22.5, V0 = 2.8;
+
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded());
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+
+app.get("/add", (req, res) => {
+  res.sendFile(__dirname + "/input.html")
+})
+app.get("/clear", (req, res) => {
+  const data = {
+    "payloadData": [],
+    "paging": {
+      "total": 1,
+      "sort": "txTime",
+      "limit": 0
+    }
+  }
+  fs.writeFileSync('./data.json', JSON.stringify(data));
+  res.send("SUCCESS");
+})
+app.post("/add", (req, res) => {
+  const data = fs.readFileSync('./data.json', 'utf8');
+  const newData = JSON.parse(data);
+  const tx = moment().format('yyyy-MM-DD HH:mm:ss');
+  const {
+    innerWaterLevel,
+    outerWaterLevel,
+    gateOpenningLevel
+  } = req.body;
+  const tempInput1 = parseFloat(outerWaterLevel);
+  if (tempInput1 < B) {
+    res.send('川表水位MUST BE GREATER THAN B');
+    return;
+  }
+  const input1 = Math.round((tempInput1 - B) / A).toString(16);
+  const tempInput2 = parseFloat(innerWaterLevel);
+  if (tempInput2 < D) {
+    res.send('川裏水位MUST BE GREATER THAN D');
+    return;
+  }
+  const input2 = Math.round((tempInput2 - D) / C).toString(16);
+  let tempInput3 = (parseFloat(gateOpenningLevel) - F) / E;
+  const input3 = Math.round(tempInput3 < V0? tempInput3: (tempInput3 + V0)).toString(16);
+  if (input3 < 0) {
+    res.send('INPUT3 MUST BE GREATER THAN 0');
+    return;
+  }
+  const payload = '139ad74cc93c1f02' +  
+  ('0'.repeat(Math.max(4 - input1.length, 0)) + input1) + 
+  ('0'.repeat(Math.max(4 - input2.length, 0)) + input2) + 
+  ('0'.repeat(Math.max(4 - input3.length, 0)) + input3) + 
+  '0000'
+  const newItem = {
+    "txTime": tx,
+    "rxTime": tx,
+    "lfourId": "0001018026",
+    "rssi": "2",
+    "serviceTag": {
+        "serviceId": "S23854554"
+    },
+    "payloadData": payload
+  };
+  newData.payloadData = [newItem];
+  fs.writeFileSync('./data.json', JSON.stringify(newData));
+  res.send("SUCCESS");
+})
+app.get("/list", (req, res) => {
+  const data = fs.readFileSync('./data.json', 'utf8');
+  res.json(JSON.parse(data));
+})
+const server = app.listen(port, () => {
+  console.log("RUNNING ON PORT ", port);
+});
